940bc6a8d87e01f95814518eb0c6336e8bbff16d
Dan <dan@denchcity.com>
2023-04-29 11:30:34 +0100

    Add project files.

---
+++ DotNet-RPG.API/Controllers/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet_RPG.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CharacterController : Controller
+	{
+		private readonly List<Character> characters = new()
+		{
+			new Character(),
+			new Character {Id = 1, Name = "Sam"}
+		};
+
+		[HttpGet("GetAll")]
+		public ActionResult<List<Character>> Get()
+		{
+
+			return Ok(characters);
+
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Character> GetSingle(int id)
+		{
+
+			return Ok(characters.FirstOrDefault(x => x.Id == id));
+
+		}
+	}
+
+}
---
+++ DotNet-RPG.API/Models/RpgClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DotNet_RPG.API.Models
+{
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum RpgClass
+	{
+		Knight = 1,
+		Mage = 2,
+		Healer = 3
+	}
+}
